c3a45bb8ce7016ac590e6a7536322dfc2431d7fd
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-06-08 15:24:35 +0500

    v1

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 const ffmpeg = require("fluent-ffmpeg");
-const { getVideoDurationInSeconds } = require('get-video-duration')
+const {getVideoDurationInSeconds} = require('get-video-duration')
 const pathToFfmpeg = require("ffmpeg-static");
 const sharp = require('sharp');
 const fs = require('fs');
-
+const path = require("path");
+const asciify = require('asciify-image');
 ffmpeg.setFfmpegPath(pathToFfmpeg);
 
 
 getVideoDurationInSeconds('./public/gachi.mp4').then(async (duration) => {
+    let fileNames = [];
     const ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ".split("");
-    let charLength = ASCII_CHARS.length;
-    let interval = charLength / 256;
-    let size = 1000;
+    const interval = ASCII_CHARS.length / 256;
+    const fps = 30;
+    const size = 70;
+    ffmpeg({source: './public/gachi.mp4'})
+        .on('filenames', (filenames) => fileNames = filenames)
+        .on('end', async () => {
+            async function* asyncIterator() {
+                while (fileNames.length) {
+                    await new Promise((res) => {
+                        setTimeout(() => res(), 500);
+                    })
+                    const buffer = await sharp(path.join('screens', fileNames.shift()))
+                        .resize(size, size, {fit: "fill"})
+                        .png()
+                        .toBuffer();
+                    yield asciify(buffer, {
+                        fit: 'box',
+                        width: size,
+                        height: size
+                    });
+                }
+            }
 
-    const image = await sharp('./screens/1_page.jpg')
-        // .sharpen({ sigma: 20 })
-        .gamma()
-        .grayscale()
-        .resize(size, size,{fit: "fill"});
-    image.toFile('output.jpg');
-
-    // const pixels = await image.raw().toBuffer();
-    //
-    // const ASCII = pixels.reduce((acc, pixel, idx) =>
-    //     acc+=`${ASCII_CHARS[Math.floor(pixel * interval)]} ${(++idx%size) ? '' : '\n'}`, "")
-    //
-    //
-    // fs.writeFile("./output.txt", ASCII, () => {
-    //     console.log("done");
-    // });
-
-    // ffmpeg({ source: './public/gachi.mp4' })
-    //     .on('filenames', (filenames) => {
-    //         console.log('Created file names', filenames);
-    //     })
-    //     .on('end', () => {
-    //         console.log('Job done');
-    //     })
-    //     .on('error', (err) => {
-    //         console.log('Error', err);
-    //     })
-    // .takeScreenshots({
-    //     filename: '%s_page.jpg',
-    //     timemarks: Array.from({length: Math.floor(duration)}).map((_, i)=> i+1)
-    // }, 'screens');
+            for await (let ASCII of asyncIterator()) {
+                console.clear();
+                console.log(ASCII);
+            }
+        })
+        .on('error', (err) => {
+            console.log('Error', err);
+        })
+        .takeScreenshots({
+            filename: '%s_page.jpg',
+            timemarks: Array.from({length: Math.floor(duration * 2)}).map((_, i) => .5 * i)
+        }, 'screens');
 })
